b04a852ed401fe7619a34a1b7f4501c0a6a69da1
Emily Baird <emilym.baird@gmail.com>
2022-07-08 11:44:26 +0100

    assignment 3 complete

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -14,21 +14,23 @@ function Expenses(props) {
     setFilteredYear(selectedYear);
   };
 
+  const filteredExpenses = props.items.filter(item => item.date.getFullYear() == filteredYear);
+
   return (
-    <div>
-      <Card className="expenses">
-        <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-        {props.items.map((expense) => (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))}
-      </Card>
-    </div>
-  );
+  <div>
+    <Card className="expenses">
+      <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+      {filteredExpenses.map((expense) => (
+        <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      ))}
+    </Card>
+  </div>
+);
 };
 
 export default Expenses;
